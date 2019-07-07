a58013ffc56a1d6ec390941c6da32d6128a721d0
zhaoyuanguang <zhaoyuanguang1573.@163.com>
2019-07-07 16:41:27 +0800

    first commit

---
+++ git_test/src/com/zyg/git/TestGitDemo1.java
@@ -0,0 +1,12 @@
+package com.zyg.git;
+
+/**
+ * Author: zhaoyuanguang
+ * Date: 2019/7/7 16:37
+ * Content:
+ */
+public class TestGitDemo1 {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
