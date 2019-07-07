cf2ed22f62e5549848426f4bf05c514834548d2b
zhaoyuanguang <zhaoyuanguang1573.@163.com>
2019-07-07 16:42:56 +0800

    second commit

--- git_test/src/com/zyg/git/TestGitDemo1.java
+++ git_test/src/com/zyg/git/TestGitDemo1.java
@@ -8,5 +8,6 @@ package com.zyg.git;
 public class TestGitDemo1 {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2 ");
     }
 }
